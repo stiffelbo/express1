0caf397fffd6d0fc1f8a1831bbdf7ddc555e4fbf
stiffelbo <partygrouppl@gmail.com>
2021-05-19 12:17:06 +0200

    27.2 initial

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/user/', (req, res, next) => {
+  res.show = () => {
+    res.sendFile(path.join(__dirname, `/views/forbiden.html`));
+  };
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).show('404.html');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
