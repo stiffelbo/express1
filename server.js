e3f0fa2ca9e3e8e3c2139b429d237721fdef62e5
stiffelbo <partygrouppl@gmail.com>
2021-05-19 21:49:21 +0200

    27.4 form validation, sending file

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
+const multer  = require('multer');
+const fs = require('fs');
 
 const app = express();
+const upload = multer({dest: 'images/'});
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './images')));
+app.use(express.urlencoded({ extended: false }));
+
+//define render engine
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+//set views type to hbs
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -10,29 +22,65 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/style.css'));
+});
 
 app.use('/user/', (req, res, next) => {
-  res.show = () => {
-    res.sendFile(path.join(__dirname, `/views/forbiden.html`));
-  };
-  next();
+  res.render('forbiden');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', {name: req.params.name });
 });
 
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('home');  
 });
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('home'); 
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about'); 
+});
+
+app.get('/info', (req, res) => {
+  res.render('info'); 
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'dark' }); 
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');  
+});
+
+app.get('/style.css', (req, res) => {  
+  res.sendFile(path.join(__dirname, '/public/style.css'));
+});
+
+app.post('/contact/send-message', upload.single('imgfile'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  const imgfile = req.file;
+  if(imgfile && author && sender && title && message){
+    const newFilename = `images/${imgfile.originalname}`;
+    fs.rename(imgfile.path, newFilename, (err)=>{
+      if (err) throw err;    
+      if(author && sender && title && message) {    
+        res.render('contact', { isSent: true, response: `Message sent! Saved file: ${imgfile.originalname}`, file: `/${imgfile.originalname}`});
+      }
+    });
+  }else {      
+    res.render('contact', { isError: true, response: 'Not sent, Fill all fields and choose file!' });    
+  }
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404'); 
 })
 
 app.listen(8000, () => {
